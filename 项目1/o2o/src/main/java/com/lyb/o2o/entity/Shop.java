14eb63ba705f3ceff76a60c489a5097c1da0483c
LiuYB135 <1270652259@qq.com>
2018-06-13 22:48:54 +0800

    修改

---
+++ 项目1/o2o/src/main/java/com/lyb/o2o/entity/Shop.java
@@ -0,0 +1,127 @@
+package com.lyb.o2o.entity;
+
+import java.util.Date;
+
+public class Shop {
+	//ID
+	private Long shopId;
+	//用户ID
+	private Long ownerId;
+	//类别ID
+	private Long shopCategoryId;
+	//店名
+	private String shopName;
+	//描述
+	private String shopDesc;
+	//地址
+	private String shopAddr;
+	//联系方式
+	private String phone;
+	//门面照
+	private String shopImg;
+	//权重
+	private Integer priority;
+	//创建时间
+	private Date createTime;
+	//修改时间
+	private Date lastEditTime;
+	//状态
+	private Integer enableStatus;
+	//建议
+	private String advice;
+	//区域
+	private Area area;
+	//类别
+	private ShopCategory shopCategory;
+	public Long getShopId() {
+		return shopId;
+	}
+	public void setShopId(Long shopId) {
+		this.shopId = shopId;
+	}
+	public Long getOwnerId() {
+		return ownerId;
+	}
+	public void setOwnerId(Long ownerId) {
+		this.ownerId = ownerId;
+	}
+	public Long getShopCategoryId() {
+		return shopCategoryId;
+	}
+	public void setShopCategoryId(Long shopCategoryId) {
+		this.shopCategoryId = shopCategoryId;
+	}
+	public String getShopName() {
+		return shopName;
+	}
+	public void setShopName(String shopName) {
+		this.shopName = shopName;
+	}
+	public String getShopDesc() {
+		return shopDesc;
+	}
+	public void setShopDesc(String shopDesc) {
+		this.shopDesc = shopDesc;
+	}
+	public String getShopAddr() {
+		return shopAddr;
+	}
+	public void setShopAddr(String shopAddr) {
+		this.shopAddr = shopAddr;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getShopImg() {
+		return shopImg;
+	}
+	public void setShopImg(String shopImg) {
+		this.shopImg = shopImg;
+	}
+	public Integer getPriority() {
+		return priority;
+	}
+	public void setPriority(Integer priority) {
+		this.priority = priority;
+	}
+	public Date getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+	public Date getLastEditTime() {
+		return lastEditTime;
+	}
+	public void setLastEditTime(Date lastEditTime) {
+		this.lastEditTime = lastEditTime;
+	}
+	public Integer getEnableStatus() {
+		return enableStatus;
+	}
+	public void setEnableStatus(Integer enableStatus) {
+		this.enableStatus = enableStatus;
+	}
+	public String getAdvice() {
+		return advice;
+	}
+	public void setAdvice(String advice) {
+		this.advice = advice;
+	}
+	public Area getArea() {
+		return area;
+	}
+	public void setArea(Area area) {
+		this.area = area;
+	}
+	public ShopCategory getShopCategory() {
+		return shopCategory;
+	}
+	public void setShopCategory(ShopCategory shopCategory) {
+		this.shopCategory = shopCategory;
+	}
+	
+}
